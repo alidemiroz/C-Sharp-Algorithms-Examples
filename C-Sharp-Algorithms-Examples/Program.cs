85214482c5cc2f61aea81d69868d1aa99dae0b9a
aliDemiroz <dmrzmehmetali@gmail.com>
2024-05-02 13:47:37 +0300

    Day2

--- C-Sharp-Algorithms-Examples/Program.cs
+++ C-Sharp-Algorithms-Examples/Program.cs
@@ -7,9 +7,6 @@ namespace C_Sharp_Algorithms_Examples
     {
         static void Main(string[] args)
         {
-            //FactorialCalculationDay1();
-            //FindingPrimeNumberDay1();
-            //CreatingFibonacciArrayDay1();
             Console.ReadLine();
         }
 
@@ -97,6 +94,88 @@ namespace C_Sharp_Algorithms_Examples
                 Console.WriteLine();
             }
         }
+
+        static void FactorialCalculationDay2()
+        {
+            // I did it in 4 minutes on day 2.
+            while (true)
+            {
+                Console.Write("Sayı giriniz :");
+                int number = int.Parse(Console.ReadLine());
+
+                int temp = number;
+
+                for (int i = number - 1; i > 0; i--)
+                {
+                    temp = temp * i;
+                }
+
+                Console.WriteLine(number + " sayısının faktöriyeli: " + temp);
+            }
+        }
+
+        static void FindingPrimeNumberDay2()
+        {
+            // I did it in 6 minutes on day 2.
+            while (true)
+            {
+                Console.Write("Hangi sayıya kadar olan asal sayıları görmek istiyorsunuz: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write(number + " sayısına kadar olan asal sayılar: ");
+
+                for (int i = 2; i < number; i++)
+                {
+                    int count = 0;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        Console.Write(i + ",");
+                    }
+                }
+            }
+        }
+
+        static void CreatingFibonacciArrayDay2()
+        {
+            // I did it in 5 minutes on day 2.
+            while (true)
+            {
+                Console.Write("Kaç adetli Fibonacci dizisi istiyorsunuz: ");
+                int number = int.Parse(Console.ReadLine());
+
+                List<int> fibonacci = new List<int>();
+                fibonacci.Add(0);
+                fibonacci.Add(1);
+
+                int temp = 0;
+                int temp1 = 0;
+                int temp2 = 1;
+
+                for (int i = 0; i < number - 2; i++)
+                {
+                    temp = temp1 + temp2;
+                    fibonacci.Add(temp);
+                    temp1 = temp2;
+                    temp2 = temp;
+                }
+
+                Console.Write("Fibonacci dizisi: ");
+
+                foreach (var item in fibonacci)
+                {
+                    Console.Write("(" + item + ")");
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 }
 
